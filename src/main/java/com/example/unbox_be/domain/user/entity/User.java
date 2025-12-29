01d4ce844e8f97bb1287fd34cbc4982a4b2923a5
GyeongJoon <jjangjoon328@naver.com>
2025-12-29 20:26:59 +0900

    feat : UserEntity 구현 및 연관관계 매핑
    - User에 필요한 기본 속성 구현
    - 타 엔티티와의 연관관계 매핑 로직 구현
    - 각 속성 제약 조건 정의
    Refs #4

--- src/main/java/com/example/unbox_be/domain/user/entity/User.java
+++ src/main/java/com/example/unbox_be/domain/user/entity/User.java
@@ -5,11 +5,9 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Builder
 @Entity
 @Table(name = "p_users")
 @Getter
@@ -37,42 +35,34 @@ public class User extends BaseEntity {
 
 
     // 1. 배송지 목록
-    @Builder.Default
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Address> addresses = new ArrayList<>();
 
     // 2. 장바구니
-    @Builder.Default
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Cart cart;
 
     // 3. 위시리스트
-    @Builder.Default
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Wishlist> wishlists = new ArrayList<>();
 
     // 4. 구매 입찰 내역
-    @Builder.Default
     @OneToMany(mappedBy = "user")
     private List<BuyingBid> buyingBids = new ArrayList<>();
 
     // 5. 판매 입찰 내역
-    @Builder.Default
     @OneToMany(mappedBy = "user")
     private List<SellingBid> sellingBids = new ArrayList<>();
 
     // 6. 주문 내역 (구매자로서)
-    @Builder.Default
     @OneToMany(mappedBy = "buyer")
     private List<Order> orderedPurchases = new ArrayList<>();
 
     // 7. 주문 내역 (판매자로서)
-    @Builder.Default
     @OneToMany(mappedBy = "seller")
     private List<Order> orderedSales = new ArrayList<>();
 
     // 8. 리뷰 작성 내역
-    @Builder.Default
     @OneToMany(mappedBy = "buyer")
     private List<Review> reviews = new ArrayList<>();
 

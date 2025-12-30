84e6026c40ec1d4d93ed20e1f4dfadf5e181d0e2
nacgyun <nacgyun001@gmail.com>
2025-12-30 16:37:01 +0900

    fix : 자료형 수정
    SellingBidRepository 자료형 수정
    Close #20

--- src/main/java/com/example/unbox_be/domain/trade/repository/SellingBidRepository.java
+++ src/main/java/com/example/unbox_be/domain/trade/repository/SellingBidRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface SellingBidRepository extends JpaRepository<SellingBid, Long> {
+public interface SellingBidRepository extends JpaRepository<SellingBid, UUID> {
 
     // 특정 사용자의 판매 입찰 내역 조회
     List<SellingBid> findAllByUserId(Long userId);

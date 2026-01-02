4a2c2731fba204c4fd928195c0382618d9446c26
노준석 <joshua052698@gmail.com>
2026-01-02 12:54:50 +0900

    refact: OrderTrackingRequestDto 모든 필드 생성자 추가
    - 테스트 코드 및 객체 생성 편의를 위해 @AllArgsConstructor 어노테이션 추가
    - 클래스 레벨에서 모든 필드를 인자로 받는 생성자 자동 생성 처리
    
    Close #65

--- src/main/java/com/example/unbox_be/domain/order/dto/OrderTrackingRequestDto.java
+++ src/main/java/com/example/unbox_be/domain/order/dto/OrderTrackingRequestDto.java
@@ -1,11 +1,13 @@
 package com.example.unbox_be.domain.order.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class OrderTrackingRequestDto {
 
     @NotBlank(message = "운송장 번호를 입력해주세요.")

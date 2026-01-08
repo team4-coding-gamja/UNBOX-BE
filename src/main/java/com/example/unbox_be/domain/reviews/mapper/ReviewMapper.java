acd8ff8bf0c1420cfdae360b12477677d92a128d
GahyunSongDev <gahyun8876@gmail.com>
2026-01-08 18:45:08 +0900

    fix : review 수정

--- src/main/java/com/example/unbox_be/domain/reviews/mapper/ReviewMapper.java
+++ src/main/java/com/example/unbox_be/domain/reviews/mapper/ReviewMapper.java
@@ -1,20 +1,35 @@
 package com.example.unbox_be.domain.reviews.mapper;
 
-import com.example.unbox_be.domain.reviews.dto.ReviewResponseDto;
+import com.example.unbox_be.domain.reviews.dto.response.ReviewDetailResponseDto;
 import com.example.unbox_be.domain.reviews.entity.Review;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ReviewMapper {
-    public ReviewResponseDto toResponseDto(Review review) {
-        return ReviewResponseDto.builder()
-                .reviewId(review.getReviewId())  // 변경된 필드명 사용
-                .orderId(review.getOrder().getId())
-                .userName(review.getBuyer().getNickname()) // User 엔티티 필드 확인 필요
+    public ReviewDetailResponseDto toResponseDto(Review review) {
+        return ReviewDetailResponseDto.builder()
+                .id(review.getReviewId())  // 변경된 필드명 사용
                 .content(review.getContent())
                 .rating(review.getRating())
                 .imageUrl(review.getImageUrl())
                 .createdAt(review.getCreatedAt())
+                .order(ReviewDetailResponseDto.OrderInfo.builder()
+                        .id(review.getOrder().getId())
+                        .price(review.getOrder().getPrice())
+                        .buyer(ReviewDetailResponseDto.UserInfo.builder()
+                                .id(review.getBuyer().getId())
+                                .nickname(review.getBuyer().getNickname())
+                                .build())
+                        .productOption(ReviewDetailResponseDto.ProductOptionInfo.builder()
+                                .id(review.getOrder().getProductOption().getId())
+                                .option(review.getOrder().getProductOption().getOption())
+                                .product(ReviewDetailResponseDto.ProductInfo.builder()
+                                        .id(review.getOrder().getProductOption().getProduct().getId())
+                                        .name(review.getOrder().getProductOption().getProduct().getName())
+                                        .imageUrl(review.getOrder().getProductOption().getProduct().getImageUrl())
+                                        .build())
+                                .build())
+                        .build())
                 .build();
     }
 }

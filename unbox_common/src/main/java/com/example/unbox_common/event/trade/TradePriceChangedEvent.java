19ab88c739205952afae02260900830d88edfec6
hjun813 <hyeonjun0899@gmail.com>
2026-01-21 13:51:09 +0900

    fix : 깃 푸쉬 폴더 위치 오류

--- unbox_common/src/main/java/com/example/unbox_common/event/trade/TradePriceChangedEvent.java
+++ unbox_common/src/main/java/com/example/unbox_common/event/trade/TradePriceChangedEvent.java
@@ -1,4 +1,11 @@
 package com.example.unbox_common.event.trade;
 
-public record TradePriceChangeEvent() {
+import java.math.BigDecimal;
+import java.util.UUID;
+
+// ✅ Record 사용 (추천)
+// 1. 자동으로 모든 필드가 private final이 됨 (불변성 보장)
+// 2. 생성자, Getter, toString, equals, hashCode 자동 생성
+// 3. Lombok 어노테이션 불필요
+public record TradePriceChangedEvent( UUID productId, BigDecimal newLowestPrice) {
 }

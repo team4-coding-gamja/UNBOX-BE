908edcd2aed81b1e44e21de00b12c2ed9cd0b142
노준석 <joshua052698@gmail.com>
2026-02-04 18:32:13 +0900

    feat: Integrate LGTM observability stack, configure microservices with new application settings, and establish inter-service communication.

--- unbox_payment/src/main/java/com/example/unbox_payment/common/client/order/OrderClient.java
+++ unbox_payment/src/main/java/com/example/unbox_payment/common/client/order/OrderClient.java
@@ -1,7 +1,6 @@
 package com.example.unbox_payment.common.client.order;
 
 import com.example.unbox_payment.common.client.order.dto.OrderForPaymentInfoResponse;
-import com.example.unbox_payment.common.client.order.dto.OrderForReviewInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +11,6 @@ import java.util.UUID;
 
 @FeignClient(name = "unbox-order", contextId = "orderClient", url = "${order-service.url}", path = "/order")
 public interface OrderClient {
-
-    // 주문 조회 (리뷰용)
-    @GetMapping("/internal/orders/{id}/for-review")
-    OrderForReviewInfoResponse getOrderForReview(@PathVariable("id") UUID id);
 
     // 주문 조회 (결제용)
     @GetMapping("/internal/orders/{id}/for-payment")
